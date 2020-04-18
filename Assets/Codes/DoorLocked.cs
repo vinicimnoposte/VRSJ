992bbf4b64aaea9541f1d7ae09dd9ce1a1d01311
Reinaldo Ramos <masterrey@gmail.com>
2020-04-17 22:46:29 -0300

    Eventos sinistros

--- Assets/Codes/DoorLocked.cs
+++ Assets/Codes/DoorLocked.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class DoorLocked : MonoBehaviour
 {
     public AudioSource locksound;
-    
-   
+    public GameObject toEnable;
+    public Rigidbody rdb;
 
     public void ButtonAction()
     {
         locksound.Play();
+
+        if (toEnable)
+        {
+            toEnable.SetActive(true);
+        }
+
+        if (rdb)
+        {
+            rdb.AddForce(Vector3.up*5,ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (locksound)
+        {
+            locksound.Play();
+        }
     }
 
 }
---
+++ Assets/Codes/EventTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour
+{
+
+    public AudioSource sound;
+    public GameObject toEnable;
+    public Rigidbody rdb;
+    public bool autodestroy=false;
+
+    public Vector3 force;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        sound.Play();
+
+        if (toEnable)
+        {
+            toEnable.SetActive(true);
+        }
+
+        if (rdb)
+        {
+            rdb.AddForce(force * 5, ForceMode.Impulse);
+        }
+
+        if (autodestroy)
+        {
+            Destroy(gameObject,5);
+        }
+    }
+
+}
