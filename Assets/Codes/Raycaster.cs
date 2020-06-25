04b19742e06bbb669b8f8afff3c00ca64e3182d8
Reinaldo Ramos <masterrey@gmail.com>
2020-06-24 22:59:11 -0300

    Ajustes do projeto

--- Assets/Codes/Raycaster.cs
+++ Assets/Codes/Raycaster.cs
@@ -18,9 +18,8 @@ public class Raycaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
         // se o raio q sai da camera bate em alguma coisa
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 6))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, 6))
         {
 
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
@@ -31,7 +30,7 @@ public class Raycaster : MonoBehaviour
             //crosshair.transform.forward = hit.normal;
 
             //faz o crosshair sempre se alinhar com a camera
-            crosshair.transform.forward = transform.forward;
+            crosshair.transform.forward = -transform.forward;
 
             //se o objeto tiver tag player (iteragivel)
             if (hit.transform.gameObject.CompareTag("Player"))
